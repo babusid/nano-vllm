558f10d3d592a1451513f32b2b5be7847740e31a
Sidharth N. Babu <sidharth.n.babu@gmail.com>
2026-04-15 00:58:25 -0400

    fixed torch compile warnings

--- nanovllm/layers/layernorm.py
+++ nanovllm/layers/layernorm.py
@@ -13,7 +13,8 @@ class RMSNorm(nn.Module):
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(hidden_size))
 
-    def rms_forward(
+    @torch.compile
+    def rms_forward_2d(
         self,
         x: torch.Tensor,
     ) -> torch.Tensor:
@@ -24,7 +25,34 @@ class RMSNorm(nn.Module):
         x = x.to(orig_dtype).mul_(self.weight)
         return x
 
-    def add_rms_forward(
+    @torch.compile
+    def rms_forward_3d(
+        self,
+        x: torch.Tensor,
+    ) -> torch.Tensor:
+        orig_dtype = x.dtype
+        x = x.float()
+        var = x.pow(2).mean(dim=-1, keepdim=True)
+        x.mul_(torch.rsqrt(var + self.eps))
+        x = x.to(orig_dtype).mul_(self.weight)
+        return x
+
+    @torch.compile
+    def add_rms_forward_2d(
+        self,
+        x: torch.Tensor,
+        residual: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor]:
+        orig_dtype = x.dtype
+        x = x.float().add_(residual.float())
+        residual = x.to(orig_dtype)
+        var = x.pow(2).mean(dim=-1, keepdim=True)
+        x.mul_(torch.rsqrt(var + self.eps))
+        x = x.to(orig_dtype).mul_(self.weight)
+        return x, residual
+
+    @torch.compile
+    def add_rms_forward_3d(
         self,
         x: torch.Tensor,
         residual: torch.Tensor,
@@ -43,6 +71,18 @@ class RMSNorm(nn.Module):
         residual: torch.Tensor | None = None,
     ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor]:
         if residual is None:
-            return self.rms_forward(x)
+            if x.dim() == 2:
+                return self.rms_forward_2d(x)
+            if x.dim() == 3:
+                return self.rms_forward_3d(x)
+            raise ValueError(
+                f"RMSNorm expects rank-2 or rank-3 input, got rank-{x.dim()}"
+            )
         else:
-            return self.add_rms_forward(x, residual)
+            if x.dim() == 2:
+                return self.add_rms_forward_2d(x, residual)
+            if x.dim() == 3:
+                return self.add_rms_forward_3d(x, residual)
+            raise ValueError(
+                f"RMSNorm expects rank-2 or rank-3 input, got rank-{x.dim()}"
+            )
